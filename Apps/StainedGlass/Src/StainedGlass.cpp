30a3c0c64bcec20aed3964bd04a508a6b85689fb
Sergey Kosarevsky <sk@linderdaum.com>
2012-10-11 23:13:34 +0400

    - fixed rendering order

--- Apps/StainedGlass/Src/StainedGlass.cpp
+++ Apps/StainedGlass/Src/StainedGlass.cpp
@@ -15,7 +15,7 @@ LMatrix4  		      Trans;
 clVirtualTrackball    Trackball;
 
 void DrawCarousel(int n, float r) {	
-	float alp = 2 * M_PI / (3 * n);
+	float alp = 2 * Math::PI / (3 * n);
 	float h = 800.f / 480 * r * sin(alp) * 2;
 	bool MousePressedL = Env->Console->IsKeyPressed( LK_LBUTTON );
     Env->Viewport->UpdateTrackball( &Trackball, 10.0f, MousePressedL );
@@ -25,10 +25,10 @@ void DrawCarousel(int n, float r) {
 	Trans = Trackball.GetRotationMatrix() * LMatrix4::GetTranslateMatrix( LVector3( 0.0f, 0.0f, -5.0f ) );
 	Env->Renderer->GetCanvas()->SetMatrices(Projection, Trans);
 
+	LString file = "img0.jpg";
+	clImage* Img = Env->Resources->LoadImg(file, L_TEXTURE_2D);		
 	
 	for (int i = 0; i < n; i++) {
-		LString file = "img0.jpg";
-		clImage* Img = Env->Resources->LoadImg(file, L_TEXTURE_2D);		
 		float alpStart	= i * 3 * alp;
 		float alpEnd	= (i * 3 + 2) * alp;
 		LVector3 p00 = LVector3(r * cos(alpStart), r * sin(alpStart), -h / 2);
@@ -39,15 +39,27 @@ void DrawCarousel(int n, float r) {
 		Env->Renderer->GetCanvas()->Line3D(p10, p11, LC_Green);
 		Env->Renderer->GetCanvas()->Line3D(p11, p01, LC_Green);
 		Env->Renderer->GetCanvas()->Line3D(p01, p00, LC_Green);
-		Env->Renderer->GetCanvas()->TexturedRect3D(p00, p10, p11, p01, Img->GetTexture(), NULL, false);
-//		Env->Renderer->GetCanvas()->TexturedRect3D(p00, p10, p11, p01, NULL, Env->Resources->LoadSP("shader.sp", ""), false);
 	}    	
+
+	Env->Renderer->GetCanvas()->Flush();
+
+	for (int i = 0; i < n; i++) {
+		float alpStart	= i * 3 * alp;
+		float alpEnd	= (i * 3 + 2) * alp;
+		LVector3 p00 = LVector3(r * cos(alpStart), r * sin(alpStart), -h / 2);
+		LVector3 p10 = LVector3(r * cos(alpStart), r * sin(alpStart), h / 2);
+		LVector3 p11 = LVector3(r * cos(alpEnd), r * sin(alpEnd), h / 2);
+		LVector3 p01 = LVector3(r * cos(alpEnd), r * sin(alpEnd), -h / 2);
+		Env->Renderer->GetCanvas()->TexturedRect3D(p00, p10, p11, p01, Img->GetTexture(), NULL, true);
+		//		Env->Renderer->GetCanvas()->TexturedRect3D(p00, p10, p11, p01, NULL, Env->Resources->LoadSP("shader.sp", ""), false);
+	}    	
+
+	Env->Renderer->GetCanvas()->Flush();
 }
 
 void DrawOverlay( LEvent Event, const LEventArgs& Args )
 {
-	
-	DrawCarousel(6, 0.9);
+	DrawCarousel(6, 0.9f);
 
 	Env->Renderer->GetCanvas()->Flush();
 }
