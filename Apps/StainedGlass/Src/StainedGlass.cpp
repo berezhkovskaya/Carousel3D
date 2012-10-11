27fd009f0fe8c0b4979412145dc79f1c94caa3a7
romanandreev <romanandreev@gmail.com>
2012-10-11 22:57:59 +0400

    -Working app with rectangles

--- Apps/StainedGlass/Src/StainedGlass.cpp
+++ Apps/StainedGlass/Src/StainedGlass.cpp
@@ -1,17 +1,54 @@
 /**
  * \file StainedGlass.cpp
  * \brief StainedGlass
- * \version 1.0.00
- * \date 08/10/2012
+ * \version 1.0.01
+ * \date 11/10/2012
  */
 
 #include "Linderdaum.h"
-
+//#include <cmath>
+//using namespace std;
 sEnvironment* Env = NULL;
+
+LMatrix4              Projection;
+LMatrix4  		      Trans;
+clVirtualTrackball    Trackball;
+
+void DrawCarousel(int n, float r) {	
+	float alp = 2 * M_PI / (3 * n);
+	float h = 800.f / 480 * r * sin(alp) * 2;
+	bool MousePressedL = Env->Console->IsKeyPressed( LK_LBUTTON );
+    Env->Viewport->UpdateTrackball( &Trackball, 10.0f, MousePressedL );
+
+
+	Projection = Math::Perspective( 45.0f, Env->Viewport->GetAspectRatio(), 0.4f, 2000.0f );
+	Trans = Trackball.GetRotationMatrix() * LMatrix4::GetTranslateMatrix( LVector3( 0.0f, 0.0f, -5.0f ) );
+	Env->Renderer->GetCanvas()->SetMatrices(Projection, Trans);
+
+	
+	for (int i = 0; i < n; i++) {
+		LString file = "img0.jpg";
+		clImage* Img = Env->Resources->LoadImg(file, L_TEXTURE_2D);		
+		float alpStart	= i * 3 * alp;
+		float alpEnd	= (i * 3 + 2) * alp;
+		LVector3 p00 = LVector3(r * cos(alpStart), r * sin(alpStart), -h / 2);
+		LVector3 p10 = LVector3(r * cos(alpStart), r * sin(alpStart), h / 2);
+		LVector3 p11 = LVector3(r * cos(alpEnd), r * sin(alpEnd), h / 2);
+		LVector3 p01 = LVector3(r * cos(alpEnd), r * sin(alpEnd), -h / 2);
+		Env->Renderer->GetCanvas()->Line3D(p00, p10, LC_Green);
+		Env->Renderer->GetCanvas()->Line3D(p10, p11, LC_Green);
+		Env->Renderer->GetCanvas()->Line3D(p11, p01, LC_Green);
+		Env->Renderer->GetCanvas()->Line3D(p01, p00, LC_Green);
+		Env->Renderer->GetCanvas()->TexturedRect3D(p00, p10, p11, p01, Img->GetTexture(), NULL, false);
+//		Env->Renderer->GetCanvas()->TexturedRect3D(p00, p10, p11, p01, NULL, Env->Resources->LoadSP("shader.sp", ""), false);
+	}    	
+}
 
 void DrawOverlay( LEvent Event, const LEventArgs& Args )
 {
-	Env->Renderer->GetCanvas()->Rect( 0.2f, 0.2f, 0.8f, 0.8f, LC_Red );
+	
+	DrawCarousel(6, 0.9);
+
 	Env->Renderer->GetCanvas()->Flush();
 }
 
