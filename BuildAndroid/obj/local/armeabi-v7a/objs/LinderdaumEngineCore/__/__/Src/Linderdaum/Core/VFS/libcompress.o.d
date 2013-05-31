0f2b62cd167565e31397af288037dfc23e5ca745
Sergey Kosarevsky <sk@linderdaum.com>
2013-05-31 17:03:53 +0400

    Added precompiled libs

--- BuildAndroid/obj/local/armeabi-v7a/objs/LinderdaumEngineCore/__/__/Src/Linderdaum/Core/VFS/libcompress.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi-v7a/objs/LinderdaumEngineCore/__/__/Src/Linderdaum/Core/VFS/libcompress.o: \
- jni/../../Src/Linderdaum/Core/VFS/libcompress.c \
- jni/../../Src/Linderdaum/Core/VFS/libcompress.h
-
-jni/../../Src/Linderdaum/Core/VFS/libcompress.h:
